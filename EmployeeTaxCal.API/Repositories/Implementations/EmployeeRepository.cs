22b0efe339f4aff7b5e72256b1fd7cc65f2d0f4e
lesego.gaompe <lesego.gaompe@halocare.co.za>
2024-01-31 14:24:46 +0200

    Employee: Sync

---
+++ EmployeeTaxCal.API/Repositories/Implementations/EmployeeRepository.cs
@@ -0,0 +1,50 @@
+﻿using EmployeeTaxCal.API.Models;
+using EmployeeTaxCal.API.Repositories.Interfaces;
+
+namespace EmployeeTaxCal.API.Repositories.Implementations
+{
+    public class EmployeeRepository: IEmployee
+    {
+        private readonly EmployeeDBContext _dbContext;
+        public EmployeeRepository(EmployeeDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public int CreateEmployee(Employee employee)
+        {
+            int result = -1;
+            if (employee == null)
+            {
+                result = 0;
+            }
+            else
+            {
+                _dbContext.Employees.Add(employee);
+                _dbContext.SaveChanges();
+                result = employee.EmployeeId;
+            }
+            return result;
+        }
+
+        public int DeleteEmployee(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UpdateEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EmployeeTaxCal.API/Repositories/Interfaces/IEmployee.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeTaxCal.API.Models;
+
+namespace EmployeeTaxCal.API.Repositories.Interfaces
+{
+    public interface IEmployee
+    {
+        IEnumerable<Employee> GetAllEmployees();
+
+        Employee GetById(int id);
+        int CreateEmployee(Employee employee);
+
+        int UpdateEmployee(Employee employee);
+        int DeleteEmployee(int id);
+    }
+}
